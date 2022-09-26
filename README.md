258eed6c741b270df3c742ca72e2364824549cd4
Hyojoo Mills <hyojoo.mills@gmail.com>
2022-09-26 11:00:18 +1000

    updated READE.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # data-transfer
+
+This service is run on a java jar to read and move data from the CSV file to the database. 
+The service will only support PostgresSQL. However, it will be updated with more database options for a user.
---
+++ src/test/java/hj/project/datatransfer/DataTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package hj.project.datatransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
