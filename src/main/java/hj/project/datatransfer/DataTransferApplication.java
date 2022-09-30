aac54db9b8ed3e7fd84b7b2692be6b99471bbe01
Hyojoo Mills <hyojoo.mills@gmail.com>
2022-09-30 11:55:12 +1000

    added components to read code from the token service

--- src/main/java/hj/project/datatransfer/DataTransferApplication.java
+++ src/main/java/hj/project/datatransfer/DataTransferApplication.java
@@ -2,8 +2,10 @@ package hj.project.datatransfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("hj.project")
 public class DataTransferApplication {
 
     public static void main(String[] args) {
--- src/test/java/hj/project/datatransfer/DataTransferApplicationTests.java
+++ src/test/java/hj/project/datatransfer/DataTransferApplicationTests.java
@@ -1,20 +1,36 @@
 package hj.project.datatransfer;
 
 import hj.project.datatransfer.services.DBConnector;
+import hj.project.token.services.MainTokenizer;
+import hj.project.token.services.Tokenize;
+import hj.project.token.services.connections.PostgresConnection;
+import hj.project.token.services.hashing.Base64Hash;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(locations="classpath:application-token.properties")
 class DataTransferApplicationTests {
 
     @Autowired
     DBConnector dbConnector;
+
+
+    MainTokenizer tokenize;
+
+    @Autowired
+    PostgresConnection postgresConnection;
+    @Autowired
+    Base64Hash hashCreator;
+
 
     @Test
     void contextLoads() {
@@ -35,6 +51,18 @@ class DataTransferApplicationTests {
 
         String test = "'hi','hi2','hi3','hi4'";
 
+
+    }
+
+    @Test
+    void token(){
+
+        String original = "testsrsss";
+        tokenize = new Tokenize(postgresConnection, hashCreator);
+        tokenize.init();
+        String token = tokenize.encode(original);
+        Assert.assertEquals("3562157357135634-141357", token);
+        Assert.assertEquals(original,tokenize.decode(token));
 
     }
 
